871da493fee6d793f4c9e683fe789e979618d0ad
Rachata Wantaokaew <rachatawantaokaew@Rachatas-MacBook-Pro.local>
2018-02-07 16:04:11 +0700

    ff

---
+++ src/main/java/rmuti/tree/AppTree.java
@@ -0,0 +1,20 @@
+package rmuti.tree;
+import rmuti.ds.Node;
+import rmuti.ds.TreeDraw;
+
+public class AppTree {
+	public AppTree() {
+	}
+
+	public static void main(String[] args) {
+		Node F4 = new Node("f", null, null);
+		Node g = new Node("g", null, null);
+		Node f = new Node("f", null, null);
+		Node e = new Node("e", null, null);
+		Node c = new Node("c", g, F4);
+		Node b = new Node("b", e, f);
+		Node root = new Node("a", b, c);
+		TreeDraw drawTree = new TreeDraw(root);
+		drawTree.binaryTreeDraw();
+	}
+}
